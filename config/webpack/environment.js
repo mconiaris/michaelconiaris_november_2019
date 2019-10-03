277ca9d74594772a30936188c3a5dbbe380fc29a
mconiaris <mconiaris@gmail.com>
2019-10-03 14:12:24 -0400

    Configured webpack environment to use the Provide plugin to autoload libraries / modules and have them mapped to the ailiases.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.append('Provide',
+	new webpack.ProvidePlugin({
+		$: 'jquery',
+		jQuery: 'juery',
+		Popper: ['popper.js', 'default']
+	})
+)
+
 module.exports = environment
