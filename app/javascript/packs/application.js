ae9475a64a404577d6cf8087c76c5e38b33c0ad6
mconiaris <mconiaris@gmail.com>
2019-10-09 15:42:42 -0400

    Installed boostrap.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import 'bootstrap'
+
 
 document.addEventListener("turbolinks:load", () => {
 	$('[data-taggle="tooltip"]').tooltip()
