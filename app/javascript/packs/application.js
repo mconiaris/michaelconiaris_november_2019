897d0ffe45051d23dd41a50066e1623ab6765ebf
mconiaris <mconiaris@gmail.com>
2019-10-09 14:35:17 -0400

    Removed references to bootstrap.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,9 +16,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'bootstrap'
-import './src/application.scss'
-
 
 document.addEventListener("turbolinks:load", () => {
 	$('[data-taggle="tooltip"]').tooltip()
