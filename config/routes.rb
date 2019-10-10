780d6ebfeecdd293567eb31531bdc9d07277b76e
mconiaris <mconiaris@gmail.com>
2019-10-10 15:06:03 -0400

    Simplified routes for index, about and contact.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+	get 'home', to: 'welcome#index'
+	get 'index', to: 'welcome#index'
+	get 'about', to: 'welcome#about'
+	get 'contact', to: 'welcome#contact'
+
   root 'welcome#index'
 end
