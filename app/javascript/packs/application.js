f9c014cd93887fcce23a5f555a88b93f20ca33c9
mconiaris <mconiaris@gmail.com>
2019-10-24 16:16:52 -0400

    Removed jQuery. It was breaking my links.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,14 +18,3 @@ require("channels")
 
 import '../stylesheets/application'
 import 'bootstrap'
-
-$(document).ready(function () {
-    $('.navbar li').click(function(e) {
-        $('.navbar li').removeClass('active');
-        var $this = $(this);
-        if (!$this.hasClass('active')) {
-            $this.addClass('active');
-        }
-    e.preventDefault();
-    });         
-});
