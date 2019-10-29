5dd0dbf3ec43ed54b9ad6c297d59fbed2187e192
mconiaris <mconiaris@gmail.com>
2019-10-29 16:42:41 -0400

    Added path for random routes that sends the user to the index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-	get 'home', to: 'welcome#index'
 	get 'index', to: 'welcome#index'
 	get 'about', to: 'welcome#about'
 	get 'contact', to: 'welcome#contact'
+
+	get '*path' => redirect('/index')
 
   root 'welcome#index'
 end
