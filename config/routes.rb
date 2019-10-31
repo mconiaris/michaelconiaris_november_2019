68c95a6d7ac2272ee6c1e44d5b29ce01b4332ece
mconiaris <mconiaris@gmail.com>
2019-10-31 08:55:14 -0400

    Rearranged routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#index'
 
 	get 'index', to: 'welcome#index'
 	get 'about', to: 'welcome#about'
 	get 'contact', to: 'welcome#contact'
 
 	get '*path', to: 'welcome#index'
-
-  root 'welcome#index'
 end
