b74b74ef71f1826af5b49c5ea6811f7d70f8a579
mconiaris <mconiaris@gmail.com>
2019-11-05 11:01:44 -0500

    Created new helper method so that if root or the index_path is active, the HTML class is modified.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 	def active_class(link_path)
 		current_page?(link_path) ? "nav-link active" : "nav-link"
 	end
+
+	def root_or_active_class(link_path)
+		current_page?(link_path) || current_page?(root_path) ? "nav-link active" : "nav-link"
+	end
 end
