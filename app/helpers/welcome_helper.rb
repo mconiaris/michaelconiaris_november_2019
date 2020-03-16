9429fdd2a7b1f0f3270b2d788ba5fd0fd4e73e07
mconiaris <mconiaris@gmail.com>
2020-03-16 16:55:30 -0400

    Added more photos using helpers.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -8,4 +8,92 @@ module WelcomeHelper
 		}
 	end
 
+	def get_photo_two
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/baby-sm.jpg",
+			title: "New Life",
+			caption: "A baby looking at the world."
+		}
+	end
+
+	def get_photo_three
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/christmas_dinner.jpg",
+			title: "Christmas Dinner",
+			caption: "A family celebrates a holiday meal."
+		}
+	end
+
+	def get_photo_four
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/diego_ibanez.jpg",
+			title: "Diego",
+			caption: "A protester ponders the future."
+		}
+	end
+
+	def get_photo_five
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/his_fathers_grave.jpg",
+			title: "A Father's Grave",
+			caption: "A man pays respect to his deceased father."
+		}
+	end
+
+	def get_photo_six
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/lauren_digioia.jpg",
+			title: "Lauren Digioa",
+			caption: "A protester in Grand Central Terminal."
+		}
+	end
+
+	def get_photo_seven
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/checking_her_phone.jpg",
+			title: "Checking Her Phone",
+			caption: "A woman checks her phone."
+		}
+	end
+
+	def get_photo_eight
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/power.jpg",
+			title: "Power",
+			caption: "A gravesite in Queens, NY."
+		}
+	end
+
+	def get_photo_nine
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/having_a_smoke.jpg",
+			title: "Having a Smoke",
+			caption: "A man takes a smoke break."
+		}
+	end
+
+	def get_photo_ten
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/soccer_practice.jpg",
+			title: "Soccer Practice",
+			caption: "Preparing for a game at Red Bull Arena."
+		}
+	end
+
+	def get_photo_eleven
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/laura_brenneman.jpg",
+			title: "Laura Brenneman",
+			caption: "A singer/songwriter at a gig."
+		}
+	end
+
+	def get_photo_twelve
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/westport_ct.jpg",
+			title: "The beach.",
+			caption: "A beach at Westport, CT."
+		}
+	end
+
 end
