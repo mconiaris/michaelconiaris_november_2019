56c50b995e4e6caa6e833b23efeec2fc32144cac
mconiaris <mconiaris@gmail.com>
2020-03-16 16:20:54 -0400

    Created a helper method to send photos to photos page.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+
+	def get_photo_one
+		{ 
+			link: "https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/samples/buffalo_bills_fans.jpg",
+			title: "Cheering a Touchdown",
+			caption: "The NYC Buffalo Bills Backers cheer a touchdown at McFadden's in Manhattan."
+		}
+	end
+
 end
